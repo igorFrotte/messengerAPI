597048b3012770f10232f149ae27ad26b3cf2903
igorFrotte <igorfrotte@gmail.com>
2024-01-29 13:52:11 -0300

    models fix

--- src/main/java/com/messenger/tweet/models/TweetModel.java
+++ src/main/java/com/messenger/tweet/models/TweetModel.java
@@ -1,5 +1,39 @@
 package com.messenger.tweet.models;
 
+import com.messenger.tweet.dtos.TweetDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Tweets")
 public class TweetModel {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private UserModel user;
+
+    @Column(length = 280, nullable = false)
+    private String text;
+
+    public TweetModel(TweetDTO dto, UserModel user){
+        this.text = dto.getTweet();
+        this.user = user;
+    }
 }
--- src/main/java/com/messenger/tweet/models/UserModel.java
+++ src/main/java/com/messenger/tweet/models/UserModel.java
@@ -1,5 +1,36 @@
 package com.messenger.tweet.models;
 
-public class UserModel { 
-    
+import com.messenger.tweet.dtos.UserDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String avatar;
+
+    public UserModel(UserDTO dto) {
+        this.username = dto.getUsername();
+        this.avatar = dto.getAvatar();
+    }
 }
